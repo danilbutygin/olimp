dfda37e4d5832b9c63ebbb919c132687209bf86e
danilbutygin <73468538+danilbutygin@users.noreply.github.com>
2020-10-26 12:49:46 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,16 +1,12 @@
 ﻿#include <iostream>
 using namespace std;
 
-int n, max_, k;
-int a[101];
 
 int main()
 {
-	max_ = 0;
-	k = 0;
+	int n;
+	int a[101];
 	cin >> n;
 	for (int i = 1; i <= n; i += 1) cin >> a[i];
-	for (int i = 1; i <= n; i += 1)	if (a[i] > max_) max_ = a[i];
-	for (int i = 1; i <= n; i += 1)	if (a[i] == max_) k += 1;
-	cout << max_ << " " << k;
+	for (int i = n; i >= 1; i -= 1) cout << a[i] <<" ";
 }
